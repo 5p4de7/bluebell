960d98107c32f468bdf449d9ae13b7d47181e3b4
5p4de7 <jwj12712@gmail.com>
2025-09-16 19:47:49 +0800

    1

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func main() {
+	//*1.加载配置
+	//*2.加载日志
+	//*3.连接mysql
+	//*4.连接redis
+	//*5.注册路由
+	//*6.启动服务
+}
